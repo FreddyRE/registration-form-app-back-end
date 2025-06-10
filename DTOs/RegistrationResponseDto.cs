440cfd74db82455a4fcd871b6e716cb46bbfe19c
Freddy Romero <ing.freddy.r.e@gmail.com>
2025-06-09 23:48:26 -0500

    feat: added Model and DTOs

---
+++ DTOs/RegistrationResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RegistrationForm.DTOs
+{
+    public class RegistrationResponseDto
+    {
+        public string Message { get; set; }
+        public DateTime RegisteredAt { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistrationForm.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        public DateTime DateOfBirth { get; set; } 
+
+    }
+}
